ae958ef61366ebaea3a98c997a477b9fda0fddb4
Ken8712 <skyblow.9@gmail.com>
2025-05-09 15:22:16 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 Rails.application.routes.draw do
   root to: "messages#index"
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :destroy]
 end
